1280dfdc582d490e1d99ac11701ab0557304e615
Jose Conde <a299506@uach.mx>
2023-05-15 15:18:35 -0600

    Se añadio funcionalidad para el formulario de dato

--- server.js
+++ server.js
@@ -1,34 +1,53 @@
 const http = require('http');
 const fs = require('fs');
 
-console.log('Hola mundo');
-
 const server = http.createServer((req, res) => {
   const notFound = fs.readFileSync('./site/404.html');
   const file = req.url == '/' ? './site/landingPage.html' : `./site${req.url}`;
 
-  fs.readFile(file, (err, data) => {
-    if (err) {
-      res.writeHead(404);
-      res.write(notFound);
-      res.end();
-    } else {
-      let contentType = 'text/html';
+  if (req.method === 'POST' && req.url === '/submit') {
+    let formData = '';
 
-      if (file.endsWith('.css')) {
-        contentType = 'text/css';
-      } else if (file.endsWith('.js')) {
-        contentType = 'application/javascript';
-      } else if (file.endsWith('.jpeg') || file.endsWith('.jpg')) {
-        contentType = 'image/jpeg';
-      } else if (file.endsWith('.png')) {
-        contentType = 'image/png';
+    req.on('data', chunk => {
+      formData += chunk;
+    });
+
+    req.on('end', () => {
+      fs.appendFile('./site/info.txt', formData + '\n', err => {
+        if (err) {
+          console.error(err);
+          res.writeHead(500);
+          res.end();
+        } else {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end('<h1>¡Formulario enviado con éxito!</h1>');
+        }
+      });
+    });
+  } else {
+    fs.readFile(file, (err, data) => {
+      if (err) {
+        res.writeHead(404);
+        res.write(notFound);
+        res.end();
+      } else {
+        let contentType = 'text/html';
+
+        if (file.endsWith('.css')) {
+          contentType = 'text/css';
+        } else if (file.endsWith('.js')) {
+          contentType = 'application/javascript';
+        } else if (file.endsWith('.jpeg') || file.endsWith('.jpg')) {
+          contentType = 'image/jpeg';
+        } else if (file.endsWith('.png')) {
+          contentType = 'image/png';
+        }
+
+        res.writeHead(200, { 'Content-Type': contentType });
+        res.end(data);
       }
-
-      res.writeHead(200, { 'Content-Type': contentType });
-      res.end(data);
-    }
-  });
+    });
+  }
 });
 
 server.listen(8888, () => {
